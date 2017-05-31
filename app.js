8cf77b737bbe476f0a99356e5de073e36268a914
Chris <hayhay_co@yahoo.com>
2017-05-31 22:13:09 +0800

    tambah start proses env

--- app.js
+++ app.js
@@ -141,6 +141,6 @@ app.delete("/world/:id", function(req,res)
 
 })
 
-    app.listen(process.env.PORT|3000, function(){
+    app.listen(process.env.PORT||3000, function(){
       console.log("Server Starting");
     });
